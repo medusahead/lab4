300569931a301b8479e8774df5a713093f8b5821
joj.m <jojmorataya@gmail.com>
2018-01-31 20:24:28 -0800

    Added table of contents, project page links, new color scheme

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var hello = require('./routes/hello');
+
+var index = require('./routes/index');
+var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
@@ -37,6 +40,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
